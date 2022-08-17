b5df21f82474528bb83713090186fb71223de806
Joel Plotnik <joelplotnik@gmail.com>
2022-08-17 10:25:12 -0700

    Expose routes, add edit and update actions, add edit template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,9 +6,12 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
@@ -17,6 +20,16 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 end
